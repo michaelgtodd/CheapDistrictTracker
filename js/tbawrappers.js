7df32150748f7f8332c2519b7080485123ceb8df
Thomas Lourenco <TSLourenco@WPI.edu>
2022-04-23 13:27:42 -0400

    I changed everything and I'm still unhappy.

--- js/tbawrappers.js
+++ js/tbawrappers.js
@@ -28,10 +28,8 @@ function test(p1) {
 }
 
 function getDistricts(callback) {
-    var APICall = TBABaseURL + "/districts/2019";
-    var data = $.getJSON
-        (APICall,
-        {
+    var APICall = TBABaseURL + "/districts/2022";
+    var data = $.getJSON(APICall, {
             "X-TBA-Auth-Key": APIKey
         },
         callback);
@@ -39,17 +37,14 @@ function getDistricts(callback) {
 
 function getPitData(callback) {
     var APICall = "./pitdata.html";
-    var data = $.getJSON
-        (APICall,
+    var data = $.getJSON(APICall,
         null,
         callback);
 }
 
 function getDistrictTeams(districtkey, callback) {
     var APICall = TBABaseURL + "/district/" + year + districtkey + "/teams/simple";
-    var data = $.getJSON
-        (APICall,
-        {
+    var data = $.getJSON(APICall, {
             "X-TBA-Auth-Key": APIKey
         },
         callback);
@@ -57,63 +52,53 @@ function getDistrictTeams(districtkey, callback) {
 
 function getTeamsAtEvent(eventkey, callback) {
     var APICall = TBABaseURL + "/event/" + eventkey + "/teams/simple";
-    var data = $.getJSON
-        (APICall,
-        {
-            "X-TBA-Auth-Key": APIKey
-        },
-        callback)
-        .error(function(){callback([])});
+    var data = $.getJSON(APICall, {
+                "X-TBA-Auth-Key": APIKey
+            },
+            callback)
+        .error(function() { callback([]) });
 }
 
 function getEventRankings(eventkey, callback) {
     var APICall = TBABaseURL + "/event/" + eventkey + "/rankings";
-    var data = $.getJSON
-        (APICall,
-        {
-            "X-TBA-Auth-Key": APIKey
-        },
-        callback)
-        .error(function(){callback([])});
+    var data = $.getJSON(APICall, {
+                "X-TBA-Auth-Key": APIKey
+            },
+            callback)
+        .error(function() { callback([]) });
 }
 
 function getEventOPRs(eventkey, callback) {
     var APICall = TBABaseURL + "/event/" + eventkey + "/oprs";
-    var data = $.getJSON
-        (APICall,
-        {
-            "X-TBA-Auth-Key": APIKey
-        },
-        callback)
-        .error(function(){callback([])});
+    var data = $.getJSON(APICall, {
+                "X-TBA-Auth-Key": APIKey
+            },
+            callback)
+        .error(function() { callback([]) });
 }
 
 function getEventAwardData(eventkey, callback) {
     var APICall = TBABaseURL + "/event/" + eventkey + "/awards";
-    var data = $.getJSON
-        (APICall,
-        {
-            "X-TBA-Auth-Key": APIKey
-        },
-        callback)
-        .error(function(){callback([])});
+    var data = $.getJSON(APICall, {
+                "X-TBA-Auth-Key": APIKey
+            },
+            callback)
+        .error(function() { callback([]) });
 }
 
 function getEventPlayoffData(eventkey, callback) {
     var APICall = TBABaseURL + "/event/" + eventkey + "/alliances";
-    var data = $.getJSON
-        (APICall,
-        {
-            "X-TBA-Auth-Key": APIKey
-        },
-        callback)
-        .error(function(){callback([])});
+    var data = $.getJSON(APICall, {
+                "X-TBA-Auth-Key": APIKey
+            },
+            callback)
+        .error(function() { callback([]) });
 }
 
 function filterteamlist(teamlist1, teamlist2, eventname) {
     var matchingteams = [];
-    $.each(teamlist1, function (index, element) {
-        $.each(teamlist2, function (index2, element2) {
+    $.each(teamlist1, function(index, element) {
+        $.each(teamlist2, function(index2, element2) {
             if (element.key == element2.key) {
                 var newteam = element2;
                 newteam.division = eventname
@@ -128,13 +113,12 @@ function processFilterTeamsAttendingEvents(matchingteams, eventlist, teamlist, y
     if (typeof eventlist[0] !== 'undefined') {
         var activeevent = eventlist[0];
         eventlist.splice(0, 1);
-        setStatus("Processing event: " + year + activeevent.id);
-        getTeamsAtEvent(year + activeevent.id, function (data) {
+        setStatus("Processing Event: " + year + activeevent.id);
+        getTeamsAtEvent(year + activeevent.id, function(data) {
             matchingteams = matchingteams.concat(filterteamlist(teamlist, data, activeevent.name));
             processFilterTeamsAttendingEvents(matchingteams, eventlist, teamlist, year, callback);
         });
-    }
-    else {
+    } else {
         callback(matchingteams);
     }
 }
@@ -144,11 +128,10 @@ function filterteamsattendingevents(eventlist, teamlist, year, callback) {
 }
 
 function AddRankingToTeamObjects(rankingdata, teamlist) {
-    if(rankingdata != null)
-    {
+    if (rankingdata != null) {
         if (rankingdata.rankings != null) {
-            $.each(teamlist, function (index, element) {
-                $.each(rankingdata.rankings, function (index2, element2) {
+            $.each(teamlist, function(index, element) {
+                $.each(rankingdata.rankings, function(index2, element2) {
                     if (element.key == element2.team_key) {
                         element.ranking = element2.rank;
                         element.averagerp = element2.sort_orders[0];
@@ -164,13 +147,12 @@ function processAddRankingToTeam(eventlist, teamlist, year, callback) {
     if (eventlist[0]) {
         var activeevent = eventlist[0];
         eventlist.splice(0, 1);
-        setStatus("Processing event rankings: " + year + activeevent.id);
-        getEventRankings(year + activeevent.id, function (data) {
+        setStatus("Processing Event rankings: " + year + activeevent.id);
+        getEventRankings(year + activeevent.id, function(data) {
             augmentedteamlist = AddRankingToTeamObjects(data, teamlist);
             processAddRankingToTeam(eventlist, augmentedteamlist, year, callback);
         });
-    }
-    else {
+    } else {
         setStatus("Done processing event rankings.");
         callback(teamlist);
     }
@@ -183,7 +165,7 @@ function addRankingToTeam(eventlist, teamlist, year, callback) {
 function AddOPRToTeamObjects(rankingdata, teamlist) {
     if (rankingdata != null) {
         if (rankingdata.oprs != null) {
-            $.each(teamlist, function (index, element) {
+            $.each(teamlist, function(index, element) {
                 var attr = $(rankingdata.oprs).attr(element.key);
                 if (typeof attr !== typeof undefined && attr !== false) {
                     element.opr = attr;
@@ -198,13 +180,12 @@ function processAddOPRToTeam(eventlist, teamlist, year, callback) {
     if (eventlist[0]) {
         var activeevent = eventlist[0];
         eventlist.splice(0, 1);
-        setStatus("Processing event OPRS: " + year + activeevent.id);
-        getEventOPRs(year + activeevent.id, function (data) {
+        setStatus("Processing Event OPRS: " + year + activeevent.id);
+        getEventOPRs(year + activeevent.id, function(data) {
             augmentedteamlist = AddOPRToTeamObjects(data, teamlist);
             processAddOPRToTeam(eventlist, augmentedteamlist, year, callback);
         });
-    }
-    else {
+    } else {
         setStatus("Done processing event OPRs.");
         callback(teamlist);
     }
@@ -216,9 +197,9 @@ function addOPRToTeam(eventlist, teamlist, year, callback) {
 
 function AddAwardsToTeamObjects(awarddata, teamlist) {
     if (awarddata != null) {
-        $.each(awarddata, function (index, element) {
-            $.each(element.recipient_list, function (index2, element2) {
-                $.each(teamlist, function (index3, element3) {
+        $.each(awarddata, function(index, element) {
+            $.each(element.recipient_list, function(index2, element2) {
+                $.each(teamlist, function(index3, element3) {
                     if (element3.key == element2.team_key) {
                         if (element3.awards == null) {
                             element3.awards = [];
@@ -236,13 +217,12 @@ function processAddAwardsToTeam(eventlist, teamlist, year, callback) {
     if (eventlist[0]) {
         var activeevent = eventlist[0];
         eventlist.splice(0, 1);
-        setStatus("Processing event Awards: " + year + activeevent.id);
-        getEventAwardData(year + activeevent.id, function (data) {
+        setStatus("Processing Event Awards: " + year + activeevent.id);
+        getEventAwardData(year + activeevent.id, function(data) {
             augmentedteamlist = AddAwardsToTeamObjects(data, teamlist);
             processAddAwardsToTeam(eventlist, augmentedteamlist, year, callback);
         });
-    }
-    else {
+    } else {
         setStatus("Done processing event Awards.");
         callback(teamlist);
     }
@@ -254,10 +234,10 @@ function addAwardsToTeam(eventlist, teamlist, year, callback) {
 
 function AddPlayoffsToTeamObjects(playoffdata, teamlist) {
     if (playoffdata != null) {
-        $.each(playoffdata, function (index, element) {
+        $.each(playoffdata, function(index, element) {
             if (element.picks != null) {
-                $.each(element.picks, function (index2, element2) {
-                    $.each(teamlist, function (index3, element3) {
+                $.each(element.picks, function(index2, element2) {
+                    $.each(teamlist, function(index3, element3) {
                         if (element2 == element3.key) {
                             element3.playoffdata = { level: element.status.level, status: element.status.status, name: element.name };
                         }
@@ -273,13 +253,12 @@ function processAddPlayoffsToTeam(eventlist, teamlist, year, callback) {
     if (eventlist[0]) {
         var activeevent = eventlist[0];
         eventlist.splice(0, 1);
-        setStatus("Processing event Playoffs: " + year + activeevent.id);
-        getEventPlayoffData(year + activeevent.id, function (data) {
+        setStatus("Processing Event Playoffs: " + year + activeevent.id);
+        getEventPlayoffData(year + activeevent.id, function(data) {
             augmentedteamlist = AddPlayoffsToTeamObjects(data, teamlist);
             processAddPlayoffsToTeam(eventlist, augmentedteamlist, year, callback);
         });
-    }
-    else {
+    } else {
         setStatus("Done processing event Playoffs.");
         callback(teamlist);
     }
@@ -298,8 +277,8 @@ function addPlayoffsToTeam(eventlist, teamlist, year, callback) {
 
 function AddPitsToTeamObjects(pitdata, teamlist) {
     if (pitdata != null) {
-        $.each(teamlist, function (index, element) {
-            $.each(pitdata, function (index2, element2) {
+        $.each(teamlist, function(index, element) {
+            $.each(pitdata, function(index2, element2) {
                 if (element.key == element2.team_key) {
                     element.pit = element2.pit;
                 }
@@ -310,12 +289,12 @@ function AddPitsToTeamObjects(pitdata, teamlist) {
 }
 
 function processAddPitsToTeam(teamlist, year, callback) {
-        setStatus("Processing event Pits.");
-        getPitData(function (data) {
-            augmentedteamlist = AddPitsToTeamObjects(data, teamlist);
-            setStatus("Done processing event Pits.");
-            callback(teamlist);
-        });
+    setStatus("Processing event Pits.");
+    getPitData(function(data) {
+        augmentedteamlist = AddPitsToTeamObjects(data, teamlist);
+        setStatus("Done processing event Pits.");
+        callback(teamlist);
+    });
 }
 
 function addPitsToTeam(teamlist, year, callback) {
@@ -343,7 +322,7 @@ function DistrictTeamsAtDetroitStage6(districtteamdata, year, callback) {
 
 function DistrictTeamsAtDetroitStage5(districtteamdata, year, callback) {
     var eventlist = detroiteventlist.slice(0);
-    addPlayoffsToTeam(eventlist, districtteamdata, year, function (data) {
+    addPlayoffsToTeam(eventlist, districtteamdata, year, function(data) {
         DistrictTeamsAtDetroitStage6(data, year, callback);
     });
 }
@@ -351,33 +330,33 @@ function DistrictTeamsAtDetroitStage5(districtteamdata, year, callback) {
 function DistrictTeamsAtDetroitStage4(districtteamdata, year, callback) {
     var eventlist = detroiteventlist.slice(0);
     eventlist = eventlist.concat(einsteineventlist);
-    addAwardsToTeam(eventlist, districtteamdata, year, function (data) {
+    addAwardsToTeam(eventlist, districtteamdata, year, function(data) {
         DistrictTeamsAtDetroitStage5(data, year, callback);
     });
 }
 
 function DistrictTeamsAtDetroitStage3(districtteamdata, year, callback) {
     var eventlist = detroiteventlist.slice(0);
-    addOPRToTeam(eventlist, districtteamdata, year, function (data) {
+    addOPRToTeam(eventlist, districtteamdata, year, function(data) {
         DistrictTeamsAtDetroitStage4(data, year, callback);
     });
 }
 
 function DistrictTeamsAtDetroitStage2(districtteamdata, year, callback) {
     var eventlist = detroiteventlist.slice(0);
-    addRankingToTeam(eventlist, districtteamdata, year, function (data) {
+    addRankingToTeam(eventlist, districtteamdata, year, function(data) {
         DistrictTeamsAtDetroitStage3(data, year, callback);
     });
 }
 
 function DistrictTeamsAtDetroitStage1(districtteamdata, year, callback) {
     var eventlist = detroiteventlist.slice(0);
-    filterteamsattendingevents(eventlist, districtteamdata, year, function (data) {
+    filterteamsattendingevents(eventlist, districtteamdata, year, function(data) {
         console.log("Got into my callback")
         DistrictTeamsAtDetroitStage2(data, year, callback);
     });
 }
 
 function getDistrictTeamsAtDetroit(districtkey, year, callback) {
-    getDistrictTeams(districtkey, function (data) { DistrictTeamsAtDetroitStage1(data, year, callback);});
+    getDistrictTeams(districtkey, function(data) { DistrictTeamsAtDetroitStage1(data, year, callback); });
 }
